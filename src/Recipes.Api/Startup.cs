4a18bd0f0fa5921e24a56337fdc3d9d03019bcf4
Kaique Vieira <kaiiquevieira@gmail.com>
2019-02-15 23:02:28 -0200

    setup startup webapi

--- src/Recipes.Api/Startup.cs
+++ src/Recipes.Api/Startup.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using Recipes.Api.Mappers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Recipes.Infra.IoC;
 
 namespace Recipes.Api
 {
@@ -25,17 +22,45 @@ namespace Recipes.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddCors();
+            services.AddMvc();
+            services.AddSwaggerDocument(document =>
+            {
+                document.Title = "BookWarehouse API";
+            });
+
+            services.AddAutoMapper(mapper =>
+            {
+                mapper.AddProfile(new RecipeMap());
+                mapper.AddProfile(new IngredientMap());
+            });
+
+            RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
+           
+            app.UseCors(policy =>
+            {
+                policy.AllowAnyHeader();
+                policy.AllowAnyMethod();
+                policy.AllowAnyOrigin();
+            });
 
             app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUi3();
+        }
+
+        private static void RegisterServices(IServiceCollection services)
+        {
+            // Adding dependencies from another layers (isolated from Presentation)
+            NativeInjectorBootStrapper.RegisterServices(services);
         }
     }
 }
