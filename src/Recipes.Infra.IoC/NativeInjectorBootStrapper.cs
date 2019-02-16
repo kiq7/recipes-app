3c20d346f0a134879f16dc71549ec404f3508349
Kaique Vieira <kaiiquevieira@gmail.com>
2019-02-15 22:36:13 -0200

    Add IoC

---
+++ src/Recipes.Infra.IoC/NativeInjectorBootStrapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Recipes.Domain.Interfaces.Repository;
+using Recipes.Domain.Interfaces.Services;
+using Recipes.Domain.Services;
+using Recipes.Infra.Data.Context;
+using Recipes.Infra.Data.Repositories;
+
+namespace Recipes.Infra.IoC
+{
+    public class NativeInjectorBootStrapper
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddScoped<RecipesContext>();
+            services.AddScoped<IRecipeRepository, RecipeRepository>();
+            services.AddScoped<IRecipeService, RecipeService>();
+        }
+    }
+}
